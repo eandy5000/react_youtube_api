415b24509115925b21ce2c27665d0a81c55825c8
Andy Eccleston <eandy5000@yahoo.com>
2016-10-09 17:18:38 -0500

    turned search_bar into a class based component

--- components/search_bar.js
+++ components/search_bar.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const SearchBar = () => {
-    return <input />;
+
+//We are going to turn this into a class based component
+// const SearchBar = () => {
+//     return <input />;
+// }
+
+//class based component
+class SearchBar extends React.Component {
+    render() {
+        return <input />;
+    }
+
+
 }
 
 
