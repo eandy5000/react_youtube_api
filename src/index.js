d5a95e3989090f7eeb804f8bb4e1b85c8e779901
Andy Eccleston <eandy5000@yahoo.com>
2016-10-09 17:01:37 -0500

    added search_bar component and exported it to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//components
+import SearchBar from '../components/search_bar';
 
-var App = () => {
-    return <div>I work</div>
+
+const API_KEY = "AIzaSyAGVmHLVNgrQ4MoQKpf8RggfawNZz-NEbQ";
+
+const App = () => {
+    return (<div>
+        <SearchBar />
+    </div>);
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
