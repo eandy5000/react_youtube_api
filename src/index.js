e7675b64712b22df06b3af122f5886d981650ee6
Andy Eccleston <eandy5000@yahoo.com>
2016-10-09 16:12:36 -0500

    project basics and configuration working

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+var App = () => {
+    return <div>I work</div>
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
