38725f14a104f306d5d3abd1b0ba227a9fc57b04
Andy Eccleston <eandy5000@yahoo.com>
2016-10-12 09:18:52 -0500

    onVideoSelect works

--- src/index.js
+++ src/index.js
@@ -17,11 +17,18 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {videos: [] };
+        this.state = {
+                videos: [],
+                selectedVideo: null
+             };
 
         YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
             //alternate es6 syntax used in many examples this.setState({videos})
-            this.setState({videos : videos});
+            this.setState(
+                {
+                    videos : videos,
+                    selectedVideo: videos[0]
+                });
         });
     }
 
@@ -29,8 +36,10 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]}/>
-                <VideoList videos={this.state.videos} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
             </div>
         );
     }
