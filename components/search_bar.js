715321201129c69f3b697f8adb7ecfc600eb5428
Andy Eccleston <eandy5000@yahoo.com>
2016-10-12 10:49:53 -0500

    project up and working

--- components/search_bar.js
+++ components/search_bar.js
@@ -19,13 +19,14 @@ class SearchBar extends React.Component {
             <div>
                 <input 
                     value={this.state.term}
-                    onChange={this.onInputChange.bind(this)} />
+                    onChange={event => this.onInputChange(event.target.value)} />
             </div>    
         );
     }
 
-    onInputChange(event) {
-       this.setState({term: event.target.value});
+    onInputChange(term) {
+       this.setState({term});
+       this.props.onSearchTermChange(term)
     }
 
 
--- src/index.js
+++ src/index.js
@@ -22,7 +22,11 @@ class App extends Component {
                 selectedVideo: null
              };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term){
+                YTSearch({key: API_KEY, term: term}, (videos) => {
             //alternate es6 syntax used in many examples this.setState({videos})
             this.setState(
                 {
@@ -35,7 +39,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})}
